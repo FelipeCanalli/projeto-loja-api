7972fd33f313097e807bebfe7c305a9147f1ed6e
Felipe Galvão <canallinho@gmail.com>
2020-07-28 16:57:05 -0300

    Função para cadastrar um usuário adicionada

--- domain/usuario.php
+++ domain/usuario.php
@@ -1,6 +1,5 @@
 <?php 
-// Esse arquivo é responsável com a comunicação com o banco e a preparação com o banco
-
+    // Esse arquivo é responsável com a comunicação com o banco e a preparação com o banco
 
 class Usuario{
     public $idusuario;
@@ -8,26 +7,49 @@ class Usuario{
     public $senha;
     public $foto;
 
-    // criando construtor para inicializar a class
+    // Criando construtor para inicializar a class
     public function __construct($db){
         $this->conexao = $db;
     }
 
-    // Função para listar todos os usuários cadastrados no banco de dados
+/* -------------------------------------------------------------------------- */
 
+    // Função para listar todos os usuários cadastrados no banco de dados
     public function listar(){
         $query = "select * from tbusuario";
 
         // Será criada a variável stmt (Statement - Sentença)
         // para guardar a preparação da consulta select que será 
         // executada posteriormente
-
         $stmt = $this->conexao->prepare($query);
 
-        // executar a consulta e retornar os dados
-        
+        // Executar a consulta e retornar os dados
         $stmt->execute();
         return $stmt;
     }
+
+/* -------------------------------------------------------------------------- */
+
+             // Função para cadastrar um usuário no banco de dados
+            public function cadastro(){
+            $query = "insert into tbusuario set login=:l, senha=:s, foto=:f";
+        
+            $stmt = $this->conexao-> prepare($query);
+
+            // Encriptografar a senha com o uso de md5
+            $this->senha = md5($this->senha);
+
+            // Vamos vincular os dados que vem do app ou navegador com os
+            // campos de banco de dados (bind)
+            $stmt->bindParam(":l", $this->login);
+            $stmt->bindParam(":s", $this->senha);
+            $stmt->bindParam(":f", $this->foto);
+
+            if ($stmt->execute()){
+                return true;
+            }else{
+                return false;
+            }
+        }
 }
 ?>
---
+++ service/usuario/cadastrar.php
@@ -0,0 +1,48 @@
+<?php
+
+// Vamos construir os cabeçalhos para trabalho com a api
+header("Acess-Control-Allow-Origin:*");
+header("Content-Type:application/json;charset=utf-8");
+
+// Para efetuar o cadastro de dados no banco é preciso 
+// informar a api que essa ação irá ocorrer
+header("Access-Control-Allow-Methods:POST");
+
+include_once "../../config/database.php";
+
+include_once "../../domain/usuario.php";
+
+$database = new Database();
+$db = $database->getConnection();
+
+$usuario = new Usuario($db);
+
+/*
+O cliente irá enviar os dados no formato Json. Porém
+nós precisamos dos dados no formato php para cadastrar
+em banco de dados.
+Para realizar essa conversão iremos usar o comando json_decode.
+Assim o cliente envia os dados e estes serão convertidos para php.
+*/
+$data = json_decode(file_get_contents("php://input"));
+
+// Verificando se os dados vindos do usuário estão preenchidos
+if(!empty($data->login) && !empty($data->senha) && !empty($data->foto)){
+    $usuario->login = $data->login;
+    $usuario->senha = $data->senha;
+    $usuario->foto = $data->foto;
+
+    if($usuario->cadastro()){
+        header("HTTP/1.0 201");
+        echo json_encode(array("mensagem"=>"Usuário castrado"));
+
+    }else{
+        header("HTTP/1.0 400");
+        echo json_encode(array("mensagem"=>"Não foi possível cadastrar"));
+    }
+
+}else{
+    header("HTTP/1.0 400");
+    echo json_encode(array("mensagem"=>"Você precisa preencher todos os campos"));
+}
+?>
